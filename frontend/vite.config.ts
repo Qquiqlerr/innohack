7c6fea2b5512f4b495cb517f48b15b36db5e54be
leevayy <maoaoa7@gmail.com>
2024-09-28 12:49:15 +0300

    chore: set up eslint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    eslint({
+      cache: false,
+      include: [
+        "./src/**/*.ts",
+        "./src/**/*.tsx",
+        "./fetchers/**/*.ts",
+        "./config/**/*.ts",
+        "./i18n/**/*.ts",
+        "./*.ts",
+      ],
+      exclude: ["./src/Sandbox/**.**"],
+    }),
+  ],
+});
